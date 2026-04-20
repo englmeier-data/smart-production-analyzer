e4246a71edd2b3e99f2ac96e4e4df024e221be3b
DerFlo <fastpath@gmx.net>
2026-04-20 11:26:19 +0200

    Check if file exists // Border values variable

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+uploaded_file = st.file_uploader("CSV-Datei hochladen", type=["csv"])
+
 
 def calculate_cpk(series, lower, upper):
     mean = series.mean()
@@ -24,17 +26,35 @@ st.set_page_config(page_title="Production Dashboard", layout="wide")
 
 st.title("📊 Smart Production Analyzer")
 
-# Daten laden
-data = pd.read_csv("data/production_data_analyzed.csv")
+# Daten laden nur wenn Datei vorhanden ist, sonst Standarddaten verwenden
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+else:
+    data = pd.read_csv("data/production_data_analyzed.csv")
 
-# Grenzwerte (kann später dynamisch gemacht werden)
-SPANNUNG_LSL = 4.5
-SPANNUNG_USL = 5.5
+st.subheader("📄 Geladene Daten")
+st.dataframe(data.head())
 
-WIDERSTAND_LSL = 90
-WIDERSTAND_USL = 110
+# Spalten nur Zeigen  wenn VSV geladen wurde, sonst Fehler anzeigen
+required_columns = ["spannung", "widerstand"]
+if not all(col in data.columns for col in required_columns):
+    st.error("❌ Die CSV muss die Spalten 'spannung' und 'widerstand' enthalten!")
+    st.stop()   
+
+st.subheader("⚙️ Grenzwerte einstellen")
+
+col_settings1, col_settings2 = st.columns(2)
+
+with col_settings1:
+    SPANNUNG_LSL = st.number_input("Spannung LSL", value=4.5)
+    SPANNUNG_USL = st.number_input("Spannung USL", value=5.5)
+
+with col_settings2:
+    WIDERSTAND_LSL = st.number_input("Widerstand LSL", value=90.0)
+    WIDERSTAND_USL = st.number_input("Widerstand USL", value=110.0)
 
 
+# Cp/Cpk Berechnung
 cp_sp, cpk_sp = calculate_cpk(data["spannung"], SPANNUNG_LSL, SPANNUNG_USL)
 cp_w, cpk_w = calculate_cpk(data["widerstand"], WIDERSTAND_LSL, WIDERSTAND_USL)
 
